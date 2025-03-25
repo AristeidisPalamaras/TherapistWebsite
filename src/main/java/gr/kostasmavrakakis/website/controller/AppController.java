1010405c73a28166e23361d2f4177641e3ff93e8
Aristeidis Palamaras <arpalamaras@hotmail.com>
2025-03-25 09:40:29 +0200

    Added current page tracking. Started working on a language-change button.

--- src/main/java/gr/kostasmavrakakis/website/controller/AppController.java
+++ src/main/java/gr/kostasmavrakakis/website/controller/AppController.java
@@ -1,6 +1,7 @@
 package gr.kostasmavrakakis.website.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -9,52 +10,62 @@ import org.springframework.web.bind.annotation.RequestParam;
 public class AppController {
 
     @GetMapping("/")
-    public String homeGr() {
+    public String homeGr(Model model) {
+        model.addAttribute("currentPage", "homeGr");
         return "homeGr";
     }
 
     @GetMapping("/en")
-    public String homeEn() {
+    public String homeEn(Model model) {
+        model.addAttribute("currentPage", "homeEn");
         return "homeEn";
     }
 
     @GetMapping("/proseggisi")
-    public String approachGr() {
+    public String approachGr(Model model) {
+        model.addAttribute("currentPage", "approachGr");
         return "approachGr";
     }
 
     @GetMapping("/en/my-approach")
-    public String approachEn() {
+    public String approachEn(Model model) {
+        model.addAttribute("currentPage", "approachEn");
         return "approachEn";
     }
 
     @GetMapping("/traumatotherapeia")
-    public String traumaTherapyGr(){
+    public String traumaTherapyGr(Model model) {
+        model.addAttribute("currentPage", "traumaTherapyGr");
         return "traumaTherapyGr";
     }
 
     @GetMapping("/en/trauma-therapy")
-    public String traumaTherapyEn(){
+    public String traumaTherapyEn(Model model) {
+        model.addAttribute("currentPage", "traumaTherapyEn");
         return "traumaTherapyEn";
     }
 
     @GetMapping("/biografiko")
-    public String aboutMeGr() {
+    public String aboutMeGr(Model model) {
+        model.addAttribute("currentPage", "aboutMeGr");
         return "aboutMeGr";
     }
 
     @GetMapping("/en/about-me")
-    public String aboutMeEn() {
+    public String aboutMeEn(Model model) {
+        model.addAttribute("currentPage", "aboutMeEn");
         return "aboutMeEn";
     }
 
     @GetMapping("/epikoinonia")
-    public String contactGr() {
+    public String contactGr(Model model) {
+        model.addAttribute("currentPage", "contactGr");
         return "contactGr";
     }
 
     @GetMapping("/en/contact")
-    public String contactEn() {
+    public String contactEn(Model model) {
+        model.addAttribute("currentPage", "contactEn");
         return "contactEn";
     }
 
