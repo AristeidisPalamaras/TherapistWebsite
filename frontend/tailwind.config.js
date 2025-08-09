286f013e18a61868b437eb11e18d1f0822b3874d
Aristeidis Palamaras <arpalamaras@hotmail.com>
2025-08-09 22:32:34 +0300

    More work on responsive menu and responsive hero

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -16,9 +16,17 @@ module.exports = {
         ],
       },
       fontSize: {
-        '1.5sm': ['0.9375rem', { lineHeight: '1.375rem' }], // 15px
-        '1.5xl': ['1.375rem', { lineHeight: '2rem' }], // 22px
-        '3.5xl': ['2rem', { lineHeight: '2.5rem' }], // 32px
+        '1.5sm': ['0.9375rem', { lineHeight: '1.375rem' }], // 15px -- footer
+        
+        '1.5xl': ['1.375rem', { lineHeight: '1.875rem' }], // 22px
+
+        '2.3xl': ['1.625rem', { lineHeight: '2.125rem' }], // 26px
+        '2.6xl': ['1.75rem', { lineHeight: '2.125rem' }], // 28px
+
+        '3.3xl': ['2rem', { lineHeight: '2.375rem' }], // 32px
+        '3.6xl': ['2.125rem', { lineHeight: '2.375rem' }], // 34px
+
+        '4.2xl': ['2.375rem', { lineHeight: '2.625rem' }], // 38px
       },
       colors: {
         "menu-primary": "#D0C9C0",
@@ -27,10 +35,8 @@ module.exports = {
         "btn-secondary": "#9f9977",
       },
       height: {
-        "ribbon": "3rem",
-        "menu": "8rem",
-        "banner": "calc(80vh - 3rem - 8rem)", // % viewport height - ribbon height - menu height 
-        "banner-full": "calc(100vh - 3rem - 8rem)", // viewport height - ribbon height - menu height 
+        "banner": "calc(80vh - 3rem - 8rem)", // % viewport height - ribbon height (min 3rem) - menu height (min 8rem)
+        "banner-full": "calc(100vh - 3rem - 8rem)", // viewport height - ribbon height (min 3rem) - menu height (min 8rem)
       }
     },
   },
