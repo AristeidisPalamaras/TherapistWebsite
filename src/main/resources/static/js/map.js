1f5854d1dfc6a13785f7d3423c1b30d1337bf194
Aristeidis Palamaras <arpalamaras@hotmail.com>
2025-08-20 11:29:30 +0300

    Completed the update to the map

--- src/main/resources/static/js/map.js
+++ src/main/resources/static/js/map.js
@@ -1,7 +1,7 @@
 document.addEventListener("DOMContentLoaded", function () {
   const centerCoords = [37.9838, 23.7275];
   const zoomLevel = 16;
-  const radius = 100;
+  const radius = 150;
 
   const map = L.map('map').setView(centerCoords, zoomLevel);
 
@@ -11,14 +11,11 @@ document.addEventListener("DOMContentLoaded", function () {
     maxZoom: 20
   }).addTo(map);
 
-
-
   L.circle(centerCoords, {
-    color: '#007bff', // 1
-    fillColor: '#007bff', //1
+    color: '#007bff',
+    fillColor: '#007bff',
     fillOpacity: 0.15,
     weight: 2,
     radius: radius
-  }).addTo(map).bindPopup('Approximate office location'); //1
-
+  }).addTo(map);
 });
