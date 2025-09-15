b347ef8e9f6e980ea5ce928ab4239676754c90b3
Aristeidis Palamaras <arpalamaras@hotmail.com>
2025-09-15 17:50:21 +0300

    Improved SecurityConfig

--- src/main/java/gr/kostasmavrakakis/website/config/SecurityConfig.java
+++ src/main/java/gr/kostasmavrakakis/website/config/SecurityConfig.java
@@ -2,11 +2,22 @@ package gr.kostasmavrakakis.website.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
 
 @Configuration
 public class SecurityConfig {
+
+    @Value("${spring.security.admin.password}")
+    private String adminPassword;
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
@@ -17,5 +28,20 @@ public class SecurityConfig {
             );
 
         return http.build();
+    }
+
+    @Bean
+    public UserDetailsService users(PasswordEncoder encoder) {
+        UserDetails admin = User.builder()
+            .username("admin")
+            .password(encoder.encode(adminPassword))
+            .roles("ADMIN")
+            .build();
+        return new InMemoryUserDetailsManager(admin);
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
     }
 }
