3a137795e9453e33141ea4269705f2ee5f4c3cf1
Aristeidis Palamaras <arpalamaras@hotmail.com>
2025-10-26 11:43:32 +0200

    Strengthened log sanitizer

--- src/main/java/gr/kostasmavrakakis/website/util/InputSanitizer.java
+++ src/main/java/gr/kostasmavrakakis/website/util/InputSanitizer.java
@@ -6,11 +6,20 @@ public class InputSanitizer {
 
     public static String sanitizeHeaders(String input) {
         if (input == null) return null;
-        return input.replaceAll("[\r\n]", "").trim();
+        return input.trim().replaceAll("[\r\n]", "");
     }
 
     public static String sanitizeLogs(String input) {
         if (input == null) return null;
-        return input.replaceAll("[\r\n\t]", " ");
+
+        String output = input.trim().replaceAll("[\r\n\t]", " ");
+        
+        output = output.replaceAll("\"", "\"\"");
+
+        if (output.matches("^[=+\\-@].*")) {
+            output = "'" + output;
+        }
+
+        return output;
     }
 }
